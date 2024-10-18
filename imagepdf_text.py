0d6bca1b8fd31377bf88c6615221e8ae5360da85
Nikcenter <nikcenter.org@gmail.com>
2024-10-18 11:53:23 +0300

    Create imagepdf_text.py

---
+++ imagepdf_text.py
@@ -0,0 +1,13 @@
+from pdf2image import convert_from_path
+import pytesseract
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+# Конвертуємо PDF у зображення
+pages = convert_from_path('document.pdf', 300)
+
+# Обробляємо кожну сторінку PDF
+for page_number, page_image in enumerate(pages):
+    text = pytesseract.image_to_string(page_image, lang='eng')
+    print(f"Текст зі сторінки {page_number + 1}:")
+    print(text)
