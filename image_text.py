98b637140f971a946eaaf5e94bd08c61d29f78c9
Nikcenter <nikcenter.org@gmail.com>
2024-10-18 11:48:06 +0300

    Create image_text.py

---
+++ image_text.py
@@ -0,0 +1,16 @@
+import pytesseract
+from PIL import Image
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+# Відкриття зображення
+image = Image.open('text_image.png')
+
+# Використання Tesseract для розпізнавання тексту
+text = pytesseract.image_to_string(image, lang='ukr')
+
+# Збереження тексту у файл
+with open('recognized_text.txt', 'w', encoding='utf-8') as file:
+    file.write(text)
+
+print("Текст було збережено у файл recognized_text.txt")
