12626c7e19d054c5e59b49fa1d7d8686a3588670
Nikcenter <nikcenter.org@gmail.com>
2024-11-22 11:04:54 +0200

    Update tesseract_image.py

--- tesseract_image.py
+++ tesseract_image.py
@@ -2,7 +2,7 @@ import pytesseract
 from PIL import Image
 
 # тільки для користувачів Віндоуз, якщо ні, то видаліть цю строку
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+# pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 # Відкриття зображення
 image = Image.open('text_image.png')
